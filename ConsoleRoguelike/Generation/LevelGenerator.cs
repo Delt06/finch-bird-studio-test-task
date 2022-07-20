1b8acdc75f9768f8ac5e299361c75ca9ea621fd9
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-07-20 19:56:27 +0300

    Add level generator

---
+++ ConsoleRoguelike/Generation/LevelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleRoguelike.Gameplay;
+using ConsoleRoguelike.Gameplay.Entities;
+using ConsoleRoguelike.Shared;
+
+namespace ConsoleRoguelike.Generation;
+
+public class LevelGenerator
+{
+	private readonly Game _game;
+
+	public LevelGenerator(Game game) => _game = game;
+
+	public int MinEnemiesCount { get; set; }
+	public int MaxEnemiesCount { get; set; }
+
+	public void Generate()
+	{
+		var random = new Random();
+		var enemiesCount = random.Next(MinEnemiesCount, MaxEnemiesCount + 1);
+
+		for (var i = 0; i < enemiesCount; i++)
+		{
+			var position = new Vector2Int(
+				random.Next(_game.LevelSize.X),
+				random.Next(_game.LevelSize.Y)
+			);
+			if (_game.IsBusyAtPosition(position)) continue;
+
+			_game.Place(new Enemy(), position);
+		}
+	}
+}
--- ConsoleRoguelike/Program.cs
+++ ConsoleRoguelike/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleRoguelike.Gameplay;
 using ConsoleRoguelike.Gameplay.Entities;
 using ConsoleRoguelike.Gameplay.Rollback;
+using ConsoleRoguelike.Generation;
 using ConsoleRoguelike.InputHandling;
 using ConsoleRoguelike.Rendering;
 using ConsoleRoguelike.Shared;
@@ -16,12 +17,12 @@ var inputSystem = new InputSystem(input, rollbackCapacity);
 var rollbackSystem = new RollbackSystem(game, rollbackCapacity);
 var gameplaySystem = new GameplaySystem(game, rollbackSystem, input);
 
-// Coordinate system:
-// X+: right
-// Y+: down
-game.Place(new Enemy(), new Vector2Int(2, 4));
-game.Place(new Enemy(), new Vector2Int(3, 7));
-game.Place(new Enemy(), new Vector2Int(8, 1));
+var levelGenerator = new LevelGenerator(game)
+{
+	MinEnemiesCount = 4,
+	MaxEnemiesCount = 6,
+};
+levelGenerator.Generate();
 
 var renderer = new Renderer(game);
 renderer.Render();
